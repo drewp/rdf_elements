2369b695518f56bfc03da85ae02cec82f4496252
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-01-20 23:34:10 -0800

    add external block to  silence warning. reformats.

--- bigast-graph-browse/rollup.config.js
+++ bigast-graph-browse/rollup.config.js
@@ -9,7 +9,9 @@ const workaround_some_browser_detector = "global = window";
 
 const workaround_jsonld_expand_issue = {
   namedExports: {
-    "../../streamed-graph/node_modules/.pnpm/registry.npmjs.org/jsonld/2.0.2/node_modules/jsonld/lib/index.js": ["expand"], // fixes "expand is not exported by node_modules/jsonld/lib/index.js"
+    "../../streamed-graph/node_modules/.pnpm/registry.npmjs.org/jsonld/2.0.2/node_modules/jsonld/lib/index.js": [
+      "expand",
+    ], // fixes "expand is not exported by node_modules/jsonld/lib/index.js"
   },
 };
 
@@ -21,6 +23,14 @@ export default [
       format: "esm",
       intro: `const ${workaround_some_browser_detector}, ${workaround_jsonld_module_system_picker};`,
     },
+    external: [
+      "@polymer/polymer/polymer-element",
+      "@polymer/decorators",
+      "@polymer/iron-ajax/iron-ajax.js",
+      "@polymer/paper-radio-group/paper-radio-group.js",
+      "@polymer/paper-radio-button/paper-radio-button.js",
+      "streamed-graph",
+    ],
     plugins: [
       builtins(),
       resolve({
@@ -37,7 +47,7 @@ export default [
   {
     input: "src/demo.ts",
     output: {
-      file: "/my/site/homepage/www/rdf/browse/build/bundle.js",//"build/demo.js",
+      file: "/my/site/homepage/www/rdf/browse/build/bundle.js", //"build/demo.js",
       format: "esm",
       intro: `const ${workaround_some_browser_detector}, ${workaround_jsonld_module_system_picker};`,
     },
